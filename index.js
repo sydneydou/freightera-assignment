f6bc69a4f230b543955afa86a633a833589930c4
Sydney Douglas <sydneydouglas1@hotmail.com>
2020-02-08 19:42:05 -0800

    Create table of data for user input

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+//   $(".table").append(`<li>${value.CityName}</li>`);
+
+//append this is autocomplete options
+
 $(document).ready(function() {
-  $.ajax({
-    method: "GET",
-    url: "https://api.freightera.com/api/geolocation/mex"
-  }).done(function(data) {
-    console.log(data);
+  $("#search").on("click", function() {
+    event.preventDefault();
+
+    $("#table-heading").empty();
+    $("#table-value").empty();
+
+    console.log($("#search-input").val());
+    let searchInput = $("#search-input").val();
+
+    $.ajax({
+      method: "GET",
+      url: "https://api.freightera.com/api/geolocation/" + searchInput
+    }).done(function(data) {
+      $.each(data[0], (key, value) => {
+        $("#table-heading").append(`<th>${key}</th>`);
+        $("#table-value").append(`<td>${value}</td>`);
+      });
+    });
+    $("#search-input").val(" ");
   });
 });
