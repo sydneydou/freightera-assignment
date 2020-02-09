07b607983f9ee2f71ec065d66b4df164e1278e2f
Sydney Douglas <sydneydouglas1@hotmail.com>
2020-02-09 15:22:47 -0800

    Add autocomplete file

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-//   $(".table").append(`<li>${value.CityName}</li>`);
-
-//append this is autocomplete options
-
 $(document).ready(function() {
-  $("#search").on("click", function() {
-    event.preventDefault();
-
-    $("#table-heading").empty();
-    $("#table-value").empty();
-
-    console.log($("#search-input").val());
+  $("#search-input").on("keypress", function() {
     let searchInput = $("#search-input").val();
 
     $.ajax({
       method: "GET",
       url: "https://api.freightera.com/api/geolocation/" + searchInput
     }).done(function(data) {
-      $.each(data[0], (key, value) => {
-        $("#table-heading").append(`<th>${key}</th>`);
-        $("#table-value").append(`<td>${value}</td>`);
+      let suggestedCity = [];
+      for (let i = 0; i < data.length; i++) {
+        let cityname = data[i].CityName;
+        suggestedCity.push(cityname);
+
+        // console.log(data[i]);
+
+        delete data[i].popular;
+        delete data[i].country_full;
+      }
+
+      console.log(suggestedCity);
+
+      $("#search-input")
+        .autocomplete({
+          source: suggestedCity,
+          minLength: 0,
+          autoFocus: true
+        })
+        .focus(function() {
+          $(this).autocomplete("search", $(this).val());
+        });
+
+      $("#search").on("click", function() {
+        event.preventDefault();
+
+        $("#table-heading").empty();
+        $("#table-value").empty();
+        console.log(data);
+        $.each(data[0], (key, value) => {
+          let titles = key.replace("CityName", "City Name");
+
+          $("#table-heading").append(`<th class='title'>${titles}</th>`);
+          $("#table-value").append(`<td>${value}</td>`);
+        });
+        $("#search-input").val(" ");
       });
     });
-    $("#search-input").val(" ");
+    // .fail(() => {
+    //   $(".table").prepend(
+    //     "<p> Sorry, there was an error with your request </p>"
+    //   );
+    // });
   });
 });
