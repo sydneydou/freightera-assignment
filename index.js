d93f7645711ba06995fef59c8bbd5d40e178c85e
Sydney Douglas <sydneydouglas1@hotmail.com>
2020-02-10 12:39:20 -0800

    Add province to suggestions

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-  $("#search-input").on("keypress", function() {
+  $("#search-input").on("input", function() {
     let searchInput = $("#search-input").val();
-
     $.ajax({
       method: "GET",
       url: "https://api.freightera.com/api/geolocation/" + searchInput
     }).done(function(data) {
       let suggestedCity = [];
       for (let i = 0; i < data.length; i++) {
-        let cityname = data[i].CityName;
+        let cityname = data[i].CityName + ", " + data[i].province;
         suggestedCity.push(cityname);
 
         [
@@ -19,41 +18,44 @@ $(document).ready(function() {
           "soundex",
           "scale"
         ].forEach(key => delete data[i][key]);
+
+        console.log(searchInput);
+
+        console.log(suggestedCity);
+        if (suggestedCity) {
+          $("#search-input")
+            .autocomplete({
+              source: suggestedCity,
+              autoFocus: true
+            })
+            .focus(function() {
+              $(this).autocomplete("search", $(this).val());
+            });
+        }
       }
-
-      console.log(suggestedCity);
-
-      $("#search-input")
-        .autocomplete({
-          source: suggestedCity,
-          minLength: 0,
-          autoFocus: true
-        })
-        .focus(function() {
-          $(this).autocomplete("search", $(this).val());
-        });
-
       $("#search").on("click", function() {
         event.preventDefault();
 
+        console.log(data);
         $("#table-heading").empty();
         $("#table-value").empty();
+
         console.log(data);
-        for (let i = 0; i < data.length; i++) {
-          console.log(data[i]);
-        }
+
+        $(".search-container ").addClass("show-table");
         $.each(data[0], (key, value) => {
           let titles = key.replace("CityName", "City Name");
+
           $("#table-heading").append(`<th class='title'>${titles}</th>`);
           $("#table-value").append(`<td>${value}</td>`);
         });
         $("#search-input").val(" ");
       });
     });
-    // .fail(() => {
-    //   $(".table").prepend(
-    //     "<p> Sorry, there was an error with your request </p>"
-    //   );
-    // });
   });
+  // .fail(() => {
+  //   $(".table").prepend(
+  //     "<p> Sorry, there was an error with your request </p>"
+  //   );
+  // });
 });
