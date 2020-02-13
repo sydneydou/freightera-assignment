b049916bc2a0fd308fb98fca4ee777640c59fbba
Sydney Douglas <sydneydouglas1@hotmail.com>
2020-02-12 21:41:33 -0800

    Updates ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# freightera-assignment
+# Freightera Interview Assignment
+
+## Information
+
+For this quick assignment, I was tasked with creating a simple search page, that connects to the Freightera API, giving the user live feedback as they type. This data results are then displayed accordingly in a table, while including a hyperlink to google search each city returned.
+
+## Technologies/Libraries Used
+
+- HTML5
+- CSS
+- Javascript (JQuery)
+
+## Side Note
+
+This was an excellent challenge to execute over the past few days, and I thoroughly enjoyed this assignment as apart of the interviewing process at Freightera.
+
+### Author
+
+Sydney Douglas, https://github.com/sydneydou
--- index.js
+++ index.js
@@ -8,79 +8,84 @@ $(document).ready(function() {
     let value = " " + searchInput;
     $.ajax({
       method: "GET",
+      headers: { "Content-Type": "application/json" },
       url: "https://api.freightera.com/api/geolocation/" + value
-    }).done(function(data) {
-      let suggestedCity = [];
-      let selectInput;
-      for (let i = 0; i < data.length; i++) {
-        let cityname = data[i].CityName + ", " + data[i].province;
-        suggestedCity.push(cityname);
-        [
-          "popular",
-          "country_full",
-          "dif",
-          "per_dif",
-          "soundex",
-          "scale"
-        ].forEach(key => delete data[i][key]);
+    })
+      .done(function(data) {
+        let suggestedCity = [];
+        let selectInput;
+        for (let i = 0; i < data.length; i++) {
+          let cityname = data[i].CityName + ", " + data[i].province;
+          suggestedCity.push(cityname);
+          [
+            "popular",
+            "country_full",
+            "dif",
+            "per_dif",
+            "soundex",
+            "scale"
+          ].forEach(key => delete data[i][key]);
 
-        $("#search-input")
-          .autocomplete({
-            source: suggestedCity,
-            autoFocus: true,
-            dataType: "json",
-            minLength: 1,
-            select: function(event, ui) {
-              $("#search-input").val(ui.item.value);
+          $("#search-input")
+            .autocomplete({
+              source: suggestedCity,
+              autoFocus: true,
+              dataType: "json",
+              minLength: 1,
+              select: function(event, ui) {
+                $("#search-input").val(ui.item.value);
 
-              selectInput = $("#search-input").val();
-            }
-          })
-          .focus(function() {
-            $(this).autocomplete("search", $(this).val());
+                selectInput = $("#search-input").val();
+              }
+            })
+            .focus(function() {
+              $(this).autocomplete("search", $(this).val());
+            });
+        }
+
+        $("#search").on("click", function() {
+          event.preventDefault();
+
+          let obj;
+          let hyperlinkCity;
+          let hyperlinkProvince;
+          if (selectInput) {
+            let splt = selectInput.split(", ");
+            obj = data.find(
+              obj => obj.CityName == splt[0] && obj.province == splt[1]
+            );
+          } else {
+            obj = data[0];
+          }
+
+          $("#table-heading").empty();
+          $("#table-value").empty();
+          $("table").addClass("table");
+          $(".input-section").addClass("active-table", 1000);
+
+          if (obj) {
+            hyperlinkCity = obj.CityName;
+            hyperlinkProvince = obj.province;
+            obj[
+              "CityName"
+            ] = `<a class="value" target="_blank" href='https://www.google.com/search?q=${hyperlinkCity} ${hyperlinkProvince}'>${hyperlinkCity}</a>`;
+          }
+
+          $.each(obj || data[0], (key, value) => {
+            let titles = key.replace("CityName", "City Name");
+            $("#table-heading").append(`<th class='title'>${titles}</th>`);
+            $("#table-value").append(
+              `<td><div class="value">${value}</div></td>`
+            );
+
+            $(".search-container ").addClass("show-table");
           });
-      }
 
-      $("#search").on("click", function() {
-        event.preventDefault();
-
-        let obj;
-        let hyperlinkCity;
-        let hyperlinkProvince;
-        if (selectInput) {
-          let splt = selectInput.split(", ");
-          obj = data.find(
-            obj => obj.CityName == splt[0] && obj.province == splt[1]
-          );
-        } else {
-          obj = data[0];
-        }
-
-        $("#table-heading").empty();
-        $("#table-value").empty();
-        $("table").addClass("table");
-        $(".input-section").addClass("active-table", 1000);
-
-        if (obj) {
-          hyperlinkCity = obj.CityName;
-          hyperlinkProvince = obj.province;
-          obj[
-            "CityName"
-          ] = `<a class="value" target="_blank" href='https://www.google.com/search?q=${hyperlinkCity} ${hyperlinkProvince}'>${hyperlinkCity}</a>`;
-        }
-
-        $.each(obj || data[0], (key, value) => {
-          let titles = key.replace("CityName", "City Name");
-          $("#table-heading").append(`<th class='title'>${titles}</th>`);
-          $("#table-value").append(
-            `<td><div class="value">${value}</div></td>`
-          );
-
-          $(".search-container ").addClass("show-table");
+          $("#search-input").val("");
         });
-
-        $("#search-input").val("");
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("Error:" + textStatus);
       });
-    });
   });
 });
